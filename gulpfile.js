0410986c48d22f923a17fa634ba1c677561580da
lukesUbuntu <luke@hardiman.co.nz>
2016-04-12 20:43:08 +1200

    updated + added ff version updated gulp for dev

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,53 @@ var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
+var insert = require('gulp-insert');
 
 var mobWars = {
-    version : '0.0.0.1',
-    name : 'MOD_Mob_BETA',
-    scripts : [
-    './source/vendors/*.js',
-    './source/topload/*.js',
-    './source/settings.js',
-    './source/mod/*.js',
-    './source/launch.js'
+    version: '0.0.0.2',
+    name: 'MOD_Mob_BETA',
+    scripts: [
+        './source/vendors/*.js',
+        './source/topload/*.js',
+        './source/settings.js',
+        './source/mod/*.js',
+        './source/launch.js'
     ],
-    userScripts : [],
-    userScripts_top :  ['./lib/top.js'],
-    userScripts_bottom : ['./lib/bottom.js'],
-    devMode : false,
-    distro : '',
-    manifest :  require('./lib/manifest.json'),
-    init : function(){
+    userScripts: [],
+    userScripts_top: ['./lib/top.js'],
+    userScripts_bottom: ['./lib/bottom.js'],
+    devMode: false,
+    distro: '',
+    manifest: require('./lib/manifest.json'),
+    meta: require('./lib/firefox.meta'),
+    init: function () {
         if (this.manifest.length <= 0) return console.log("Failed to init manifest")
 
-        this.distro = this.name + '_v' + this.version ;
+        this.distro = this.name + '_v' + this.version;
 
 
         this.manifest.content_scripts[0].js[0] = (this.devMode == true) ? this.distro + '.user.js' : this.distro + '.min.user.js';
 
-            //this.manifest.content_scripts[0].js[0] = this.distro + 'min.user.js';
+        //this.manifest.content_scripts[0].js[0] = this.distro + 'min.user.js';
 
         this.userScripts = this.userScripts_top.concat(this.scripts).concat(this.userScripts_bottom);
-       // this.userScripts.push(this.userScripts_bottom);
 
+        //render the ff meta
+        var meta = fs.readFileSync(__dirname + '/lib/firefox.meta', 'utf8');
 
+        // this.userScripts.push(this.userScripts_bottom);
+        var re = /{(.*?)}/gm;
+
+        //update the meta data from manifest
+        while ((m = re.exec(meta)) !== null) {
+            var key = m[1], to_replace = m[0]
+
+            if (this.manifest[key])
+                meta = meta.replace(to_replace, this.manifest[key]);
+
+        }
+
+        this.meta = meta;
 
 
     }
@@ -45,30 +61,32 @@ mobWars.init();
 
 
 gulp.task('buildUserScript', function () {
-    fs.readFile(__dirname + '/source/menu.html', function(err, data) {
+    fs.readFile(__dirname + '/source/menu.html', function (err, data) {
         //var base64data = new Buffer(data).toString('base64');
         var theme = "MOD.mainMenu = '" + new Buffer(data).toString('base64') + "'";
 
         fs.writeFile(__dirname + '/source/topload/mainmenu.js', theme, {
             flag: "w"
         }, function (err) {
-            if (err) return console.log("buildUserScript",err);
+            if (err) return console.log("buildUserScript", err);
 
-            console.log("created",mobWars.manifest.content_scripts[0].js[0]);
+            console.log("created", mobWars.manifest.content_scripts[0].js[0]);
 
             if (mobWars.devMode == true)
-               return gulp.src(mobWars.userScripts)
-                    .pipe(concat(mobWars.manifest.content_scripts[0].js[0]))
-                    .pipe(gulp.dest('./build/chromeExtension/'))
-                    .pipe(gulp.dest('./build/userscript/'));
-
-
-
                 return gulp.src(mobWars.userScripts)
                     .pipe(concat(mobWars.manifest.content_scripts[0].js[0]))
-                    .pipe(uglify())
+                    .pipe(insert.prepend(mobWars.meta))
                     .pipe(gulp.dest('./build/chromeExtension/'))
                     .pipe(gulp.dest('./build/userscript/'));
+
+
+            return gulp.src(mobWars.userScripts)
+                .pipe(concat(mobWars.manifest.content_scripts[0].js[0]))
+
+                .pipe(uglify())
+                .pipe(insert.prepend(mobWars.meta))
+                .pipe(gulp.dest('./build/chromeExtension/'))
+                .pipe(gulp.dest('./build/userscript/'));
 
 
 
@@ -78,7 +96,7 @@ gulp.task('buildUserScript', function () {
 
 });
 gulp.task('buildMenu', function () {
-    fs.readFile(__dirname + '/source/menu.html', function(err, data) {
+    fs.readFile(__dirname + '/source/menu.html', function (err, data) {
         //var base64data = new Buffer(data).toString('base64');
         var theme = "MOD.mainMenu = '" + new Buffer(data).toString('base64') + "'";
 
@@ -90,7 +108,7 @@ gulp.task('buildMenu', function () {
         });
     });
 });
-gulp.task('buildDistro',['buildMenu'], function () {
+gulp.task('buildDistro', ['buildMenu'], function () {
 
     return gulp.src(mobWars.scripts)
         .pipe(concat(mobWars.name + '_distro.js'))
@@ -118,7 +136,7 @@ gulp.task('chromeExtension', function () {
 
 //clean build directory
 gulp.task('clean', function () {
-    return gulp.src(['build/*', 'distro/*','source/topload/mainmenu.js'], {read: false})
+    return gulp.src(['build/*', 'distro/*', 'source/topload/mainmenu.js'], {read: false})
         .pipe(clean());
 });
 
@@ -133,13 +151,12 @@ gulp.task('buildChrome', ['chromeExtension'], function (cb) {
 });
 
 
-
 gulp.task('watch', function () {
     /*
-    watch(mobWars.scripts, function () {
-        gulp.run(['buildUserScript']);
-    });
-    */
+     watch(mobWars.scripts, function () {
+     gulp.run(['buildUserScript']);
+     });
+     */
     watch('./source/**/*.js', function () {
         gulp.run(['buildUserScript']);
     });
@@ -168,7 +185,7 @@ gulp.task('default', ['clean'], function () {
         gulp.start('watch');
     }
 
-    gulp.start('buildDistro');
+    //gulp.start('buildDistro');
 });
 
 function getArg(key) {
